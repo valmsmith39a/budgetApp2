ff2c01f19d7480b3879d99f260824cc2e3ddb31f
valmsmith39a <valmsmith39a@gmail.com>
2016-02-21 16:15:47 -0800

    Updates

--- main.js
+++ main.js
@@ -19,31 +19,28 @@ var filteredArrayOfRowObjectsG = [];
 function init(){
 	$('#add-entry-button').on('click', addEntryButton);
 	$('#filter-dropdown-menu').on('click', filterTransactionsButton);	
-	// $('.old-transactions').on('click', deleteEntryButton);
 }
 
 function deleteEntryButton(){
 	var $this = $(this);
 	var $rowTransaction = $this.closest('.row-container');
 	$rowTransaction.remove(); 
-	// console.log('array of row objects before delete', arrayOfRowObjectsG);
 	arrayOfRowObjectsG.slice(0,arrayOfRowObjectsG.length); // Clear the array 
 	arrayOfRowObjectsG = $('.old-transactions').find('.row-container'); 
-	// console.log('array of row objects', arrayOfRowObjectsG);
 }
 
 function filterTransactionsButton(event){
 	var filterDropdownValue = document.getElementById("filter-dropdown-menu").value;
 	filteredArrayOfRowObjectsG = arrayOfRowObjectsG.filter(function(item, index){
-            if(filterDropdownValue === 'debit') {
-            	return item.attr('id') === 'debit';
-            }
-            else if(filterDropdownValue === 'credit') {
-            	return item.attr('id') === 'credit';
-            }
-            else if(filterDropdownValue === 'all') {
-            	return item; 
-            }
+    if(filterDropdownValue === 'debit') {
+    	return item.attr('id') === 'debit';
+    }
+    else if(filterDropdownValue === 'credit') {
+    	return item.attr('id') === 'credit';
+    }
+    else if(filterDropdownValue === 'all') {
+    	return item; 
+    }
 	});
 	displayFilteredEntries(filteredArrayOfRowObjectsG);
 }
@@ -59,37 +56,36 @@ function addEntryButton(){
 	var $this = $(this);
 	dateStringG = momentG.format($('#date-input').val(), 'MM/DD/YYYY');
 	descriptionStringG = $('#description-input').val();
-    debitG = $('#debit-input').val();
-    creditG = $('#credit-input').val();
-    displayNewEntry();
+  debitG = $('#debit-input').val();
+  creditG = $('#credit-input').val();
+  displayNewEntry();
 }
 
 function displayNewEntry() {
 	var $rowContainer = $('<div>').addClass('row row-container');
-    var $dateColumn = $('<div>').addClass('col-md-2').attr('id', 'date-col-output').text(dateStringG)
-    $rowContainer.append($dateColumn); 
-    var $descriptionColumn = $('<div>').addClass('col-md-2').attr('id', 'description-col-output').text(descriptionStringG);
+  var $dateColumn = $('<div>').addClass('col-md-2').attr('id', 'date-col-output').text(dateStringG)
+  $rowContainer.append($dateColumn); 
+  var $descriptionColumn = $('<div>').addClass('col-md-2').attr('id', 'description-col-output').text(descriptionStringG);
 	$rowContainer.append($descriptionColumn);
 	var $debitColumn = $('<div>').addClass('col-md-2').attr('id', 'debit-col-output').text('$' + Number(debitG).toFixed(2));
 	$rowContainer.append($debitColumn);
 	var $creditColumn = $('<div>').addClass('col-md-2').attr('id', 'credit-col-output').text('$' + Number(creditG).toFixed(2));
-    $rowContainer.append($creditColumn);
-    var $balanceColumn = $('<div>').addClass('col-md-2').attr('id','balance-col-output').text('$'+ Number(calculateBalance()).toFixed(2));
-    $rowContainer.append($balanceColumn);
-    var $deleteIconColumn = $('<div>').addClass('delete-btn col-md-2');
-    var $deleteIcon = $('<i>').addClass('fa fa-trash');
-    $deleteIconColumn.append($deleteIcon);
-    $rowContainer.append($deleteIconColumn);
-    $deleteIconColumn.click(deleteEntryButton);	
-    if(Number(debitG) !== 0){
-    	debugger;
-    	$rowContainer.attr('id','debit');
-    }
-    else if(Number(creditG) !== 0){
-    	debugger;
-    	$rowContainer.attr('id','credit');
-    }
-    arrayOfRowObjectsG.push($rowContainer);
+  $rowContainer.append($creditColumn);
+  var $balanceColumn = $('<div>').addClass('col-md-2').attr('id','balance-col-output').text('$'+ Number(calculateBalance()).toFixed(2));
+  $rowContainer.append($balanceColumn);
+  var $deleteIconColumn = $('<div>').addClass('delete-btn col-md-2');
+  var $deleteIcon = $('<i>').addClass('fa fa-trash');
+  $deleteIconColumn.append($deleteIcon);
+  $rowContainer.append($deleteIconColumn);
+  $deleteIconColumn.click(deleteEntryButton);	
+  
+  if(Number(debitG) !== 0){
+  	$rowContainer.attr('id','debit');
+  }
+  else if(Number(creditG) !== 0){
+  	$rowContainer.attr('id','credit');
+  }
+  arrayOfRowObjectsG.push($rowContainer);
 	$('.old-transactions').append($rowContainer);
 	creditG = 0; 
 	debitG = 0; 
---
+++ readme.md
@@ -0,0 +1,1 @@
+Initial version completed: 1/13/2016. App to track budget. 
